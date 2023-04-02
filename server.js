d5bce61e9158f1dc52c058d609ca955756bcbcf6
amine <aminerate16@gmail.com>
2023-04-02 03:08:17 +0100

    production

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(passport.session());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://aviaire-api.onrender.com",
   methods: "GET,POST,PUT,DELETE",
   credentials: true,
 }));
